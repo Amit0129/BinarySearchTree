4cca1f214469753396aa041f5d3d7fdb3a1d03dd
Amit0129 <amitkumarnayak40@gmail.com>
2022-12-12 21:15:59 +0530

    create a BST by adding 56 and then adding 30 & 70

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree");
+
+            BinarySearchTreeBuilder<int> binarySearchTree = new BinarySearchTreeBuilder<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Display();
         }
     }
 }
